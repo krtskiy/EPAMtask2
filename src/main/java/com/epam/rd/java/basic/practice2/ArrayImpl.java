b21a9824e622f5d789ec24ff867fd5217457b8e9
jMushmyH <jMushmyH@email.com>
2020-07-08 17:08:59 +0300

    1

--- src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/ArrayImpl.java
@@ -3,10 +3,13 @@ package com.epam.rd.java.basic.practice2;
 import java.util.Iterator;
 
 public class ArrayImpl implements Array {
-    private Object[] objArray;
+    int arrCapacity;
+    private Object[] objArray = new Object[arrCapacity];
+    int ind;
 
-    public ArrayImpl(int arrLength) {
-        objArray = new Object[arrLength];
+    public ArrayImpl(int arrCapacity) {
+        this.objArray = objArray;
+        this.arrCapacity = arrCapacity;
     }
 
     @Override
@@ -28,19 +31,38 @@ public class ArrayImpl implements Array {
 
         @Override
         public boolean hasNext() {
+            if (ind < objArray.length) {
+                return true;
+            } else {
             return false;
+            }
         }
 
         @Override
         public Object next() {
-            return null;
+            if (hasNext()) {
+            return objArray[ind++];
+            } else {
+                return null;
+            }
         }
 
     }
 
     @Override
     public void add(Object element) {
-
+        Object[] temp = objArray;
+        objArray = new Object[temp.length + 1];
+        objArray = temp.clone();
+        objArray[objArray.length - 1] = element;
+//        Object[] addObjArray = new Object[objArray.length+1];
+//        for (int i = 0, k = 0; i < objArray.length+1; i++) {
+//            if (k < objArray.length+1) {
+//                addObjArray[k++] = objArray[i];
+//            } else {
+//                addObjArray[k] = element;
+//            }
+//        }
     }
 
     @Override
@@ -61,12 +83,12 @@ public class ArrayImpl implements Array {
 
     @Override
     public void remove(int index) {
-        Object[] anotherObjArray = new Object[objArray.length - 1];
+        Object[] removeObjArray = new Object[objArray.length - 1];
         for (int i = 0, k = 0; i < objArray.length; i++) {
             if (i == index) {
                 continue;
             } else {
-                anotherObjArray[k++] = objArray[i];
+                removeObjArray[k++] = objArray[i];
             }
         }
     }
@@ -76,7 +98,7 @@ public class ArrayImpl implements Array {
         StringBuilder str = new StringBuilder("[");
         for (int i = 0; i < objArray.length; i++) {
             if (i < objArray.length) {
-                str.append(objArray[i]);
+                str.append(objArray[i] + ", ");
             } else {
                 str.append(objArray[i] + "]");
             }
@@ -85,7 +107,7 @@ public class ArrayImpl implements Array {
     }
 
     public static void main(String[] args) {
-
+        
     }
 
 }
